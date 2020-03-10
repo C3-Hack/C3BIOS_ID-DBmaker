1d5aad2c95cb85ada02b25e8bbf06d287dfc8570
ganchan <43211384+takuya-iwanaga@users.noreply.github.com>
2020-03-10 20:31:59 +0900

    登録画面のプログラム

---
+++ python/Register.py
@@ -0,0 +1,85 @@
+import wx
+
+####エラー画面のクラス
+class Error(wx.Frame):
+    def __init__(self,parent):
+        ##フレーム
+        wx.Frame.__init__(self,parent,-1,"エラー",pos=(100,100),size=(600,300))
+        panel = wx.Panel(self)
+
+        ##テキスト
+        font = wx.Font(40, wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+        self.errortext = wx.StaticText(panel,-1, 'すでに登録されています！',pos=(50,100))
+        self.errortext.SetFont(font)
+        ##閉じるボタン
+        self.exitBtn = wx.Button(panel,label="卵で閉じる",pos=(500,250))
+        self.Bind(wx.EVT_BUTTON,self.exit,self.exitBtn)
+
+    ##ボタン実装
+    def exit(self,event):
+        self.Close(True)
+
+####登録完了の画面
+class tourokuC(wx.Frame):
+    def __init__(self,parent):
+        ##フレーム
+        wx.Frame.__init__(self,parent,1,"登録完了",pos=(300,300),size=(600,300))
+        panel = wx.Panel(self)
+
+        ##テキスト
+        font = wx.Font(40, wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+        self.tourokuCtext = wx.StaticText(panel,-1, '登録完了！',pos=(50,100))
+        self.tourokuCtext.SetFont(font)
+        ##閉じるボタン
+        self.exitBtn = wx.Button(panel,label="息の根を止める",pos=(500,250))
+        self.Bind(wx.EVT_BUTTON,self.exit,self.exitBtn)
+
+    def exit(self,event):
+        self.Close(True)
+
+####登録画面のクラス
+class Touroku(wx.Frame):
+    def __init__(self,parent,id):
+        ##フレーム
+        wx.Frame.__init__(self,parent,id,"登録画面",size=(600,600))
+        panel = wx.Panel(self)
+        font = wx.Font(40, wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+        ##テキスト
+        self.IDmtext=wx.StaticText(panel,-1, 'IDm',pos=(50,100))
+        self.studentnumbertext=wx.StaticText(panel,-1, 'SID',pos=(50,200))
+        self.handlenametext=wx.StaticText(panel,-1, 'HN',pos=(50,300))
+
+        self.studentnumbertext.SetFont(font)
+        self.handlenametext.SetFont(font)
+        self.IDmtext.SetFont(font)
+        ##入力フォーム
+        self.IDm= wx.TextCtrl(panel, -1,pos=(150,100),size=(300,60))
+        self.studentnumber = wx.TextCtrl(panel, -1,pos=(150,200),size=(300,60))
+        self.handlename= wx.TextCtrl(panel, -1,pos=(150,300),size=(300,60))
+
+        self.IDm.SetFont(font)
+        self.studentnumber.SetFont(font)
+        self.handlename.SetFont(font)
+        ##ボタン
+        a=1
+        self.button_1 = wx.Button(panel,-1, '登録する',pos=(500,500))
+        if(a==1):
+            self.Bind(wx.EVT_BUTTON,self.showChild,self.button_1)
+        else:
+            self.Bind(wx.EVT_BUTTON,self.showChild2,self.button_2)
+
+    def showChild(self,event):
+        childFrame = Error(self)
+        childID = childFrame.Show()
+
+    def showChild2(self,event):
+        childFrame1=tourokuC(self)
+        childID=childFrame1.Show()
+
+
+####実装
+if __name__ == '__main__':
+    app = wx.PySimpleApp()
+    frame = Touroku(parent=None,id=-1)
+    frame.Show()
+    app.MainLoop()
