a856daab432150bfe4946ba9c154098ffca9c931
ganchan <43211384+takuya-iwanaga@users.noreply.github.com>
2020-03-10 20:40:17 +0900

    登録画面のプログラム

--- python/Register.py
+++ python/Register.py
@@ -1,4 +1,6 @@
 import wx
+import csv
+import pprint
 
 ####エラー画面のクラス
 class Error(wx.Frame):
@@ -20,7 +22,7 @@ class Error(wx.Frame):
         self.Close(True)
 
 ####登録完了の画面
-class tourokuC(wx.Frame):
+class tourokuC(wx.Frame,):
     def __init__(self,parent):
         ##フレーム
         wx.Frame.__init__(self,parent,1,"登録完了",pos=(300,300),size=(600,300))
@@ -43,6 +45,7 @@ class Touroku(wx.Frame):
         ##フレーム
         wx.Frame.__init__(self,parent,id,"登録画面",size=(600,600))
         panel = wx.Panel(self)
+        panel.SetBackgroundColour('#AFAFAF')
         font = wx.Font(40, wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
         ##テキスト
         self.IDmtext=wx.StaticText(panel,-1, 'IDm',pos=(50,100))
@@ -60,21 +63,55 @@ class Touroku(wx.Frame):
         self.IDm.SetFont(font)
         self.studentnumber.SetFont(font)
         self.handlename.SetFont(font)
+
+
+
         ##ボタン
-        a=1
         self.button_1 = wx.Button(panel,-1, '登録する',pos=(500,500))
-        if(a==1):
-            self.Bind(wx.EVT_BUTTON,self.showChild,self.button_1)
-        else:
-            self.Bind(wx.EVT_BUTTON,self.showChild2,self.button_2)
+        self.Bind(wx.EVT_BUTTON,self.check,self.button_1)
+
+    def check(self,event):
+         ##入力を取得
+        self.IDm1=self.IDm.GetValue()
+        self.studentnumber1=self.studentnumber.GetValue()
+        self.handlename1=self.handlename.GetValue()
+        ## csvの読み込み
+        flag=0
+        with open('data.csv') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                ##登録されているか検査
+                if(row[0]==self.IDm1):
+                    flag=-1
+                    break
+                else:
+                    if(row[1]==self.studentnumber1):
+                        flag=-1
+                        break
+                    else:
+                        if(row[2]==self.handlename1):
+                            flag=-1
+                            break
+                        else:
+                            flag=1
+                            ##エラー検出
+            if(flag<0):
+                self.showChild(self)
+            ##登録
+            elif(flag==1):
+            ##csvに書き込む
+                with open('data.csv','a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow([self.IDm1,self.studentnumber1,self.handlename1])
+                self.showChild2(self)
 
     def showChild(self,event):
         childFrame = Error(self)
         childID = childFrame.Show()
 
     def showChild2(self,event):
-        childFrame1=tourokuC(self)
-        childID=childFrame1.Show()
+        childFrame2=tourokuC(self)
+        childID2=childFrame2.Show()
 
 
 ####実装
